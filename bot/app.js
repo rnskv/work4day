05509951d679f810bf4dc170a130e36c5247de9d
rnskv <web.rnskv@gmail.com>
2019-06-24 20:06:08 +0300

    Prettier non-src files

--- bot/app.js
+++ bot/app.js
@@ -1,124 +1,141 @@
-const VK = require('vk-io').VK;
+const VK = require("vk-io").VK;
 const vk = new VK();
-const request = require('request');
+const request = require("request");
 
-vk.token = '809559a9809559a9809559a93180ff209588095809559a9dc417f0517bae525d706efd3';
+vk.token =
+  "809559a9809559a9809559a93180ff209588095809559a9dc417f0517bae525d706efd3";
 
 const config = {
-    updateTime: 15 * 1000,
-    api: {
-        host: 'localhost',
-        port: 1337,
-        protocol: 'http'
-    }
+  updateTime: 15 * 1000,
+  api: {
+    host: "localhost",
+    port: 1337,
+    protocol: "http"
+  }
 };
 
 async function updateLastPost(groupId, set) {
-    request({
-        method: 'PATCH',
-        url: `${config.api.protocol}://${config.api.host}:${config.api.port}/groups/${groupId}`,
-        headers: {
-            "content-type": "application/json",  // <--Very important!!!
-        },
-        body: JSON.stringify({
-            set: set
-        })
-    }, () => {
-        console.log('All ok')
-    });
+  request(
+    {
+      method: "PATCH",
+      url: `${config.api.protocol}://${config.api.host}:${config.api.port}/groups/${groupId}`,
+      headers: {
+        "content-type": "application/json" // <--Very important!!!
+      },
+      body: JSON.stringify({
+        set: set
+      })
+    },
+    () => {
+      console.log("All ok");
+    }
+  );
 }
 
 async function getGroups() {
-    return new Promise((res, rej) => {
-        request({
-            method: 'GET',
-            url: `${config.api.protocol}://${config.api.host}:${config.api.port}/groups`,
-            headers: {
-                "content-type": "application/json",  // <--Very important!!!
-            },
-        }, (err, response, body) => {
-            try {
-                res(JSON.parse(body))
-            } catch (err) {
-
-            }
-        });
-    })
+  return new Promise((res, rej) => {
+    request(
+      {
+        method: "GET",
+        url: `${config.api.protocol}://${config.api.host}:${config.api.port}/groups`,
+        headers: {
+          "content-type": "application/json" // <--Very important!!!
+        }
+      },
+      (err, response, body) => {
+        try {
+          res(JSON.parse(body));
+        } catch (err) {}
+      }
+    );
+  });
 }
 
 async function parseGroupWall(params) {
-    const groupId = params.groupId;
+  const groupId = params.groupId;
 
-    const response = await vk.api.wall.get({
-        owner_id: groupId * -1,
-        count: 2,
-        extended: true,
-        fields: ['photo_100', 'screen_name']
-    });
-    return {
-        items: response.items,
-        group: response.groups[0]
-    };
+  const response = await vk.api.wall.get({
+    owner_id: groupId * -1,
+    count: 2,
+    extended: true,
+    fields: ["photo_100", "screen_name"]
+  });
+  return {
+    items: response.items,
+    group: response.groups[0]
+  };
 }
 
 function fetchAPI(body) {
-    request({
-        method: 'POST',
-        url: `${config.api.protocol}://${config.api.host}:${config.api.port}/vacancies`,
-        headers: {
-            "content-type": "application/json",  // <--Very important!!!
-        },
-        body: JSON.stringify(body)
-    }, () => {
-        console.log('All ok')
-    });
+  request(
+    {
+      method: "POST",
+      url: `${config.api.protocol}://${config.api.host}:${config.api.port}/vacancies`,
+      headers: {
+        "content-type": "application/json" // <--Very important!!!
+      },
+      body: JSON.stringify(body)
+    },
+    () => {
+      console.log("All ok");
+    }
+  );
 }
 
 async function fetchNews() {
-    const groups = await getGroups();
-    groups.forEach(async group => {
-        const response = await parseGroupWall({groupId: group.id});
-        const posts = response.items;
-        const newPosts = [];
-        posts.forEach(async post => {
-            if (post.id > group.lastPostId) {
-                newPosts.push(post);
+  const groups = await getGroups();
+  groups.forEach(async group => {
+    const response = await parseGroupWall({ groupId: group.id });
+    const posts = response.items;
+    const newPosts = [];
+    posts.forEach(async post => {
+      if (post.id > group.lastPostId) {
+        newPosts.push(post);
 
-                fetchAPI({
-                    groupId: group.id,
-                    postId: post.id,
-                    text: post.text,
-                    date: post.date
-                });
-
-                console.log(post.date)
-
-                group.lastParsedId = post.id;
-
-                console.log('updateLastPost');
-
-                await updateLastPost(group.id, {
-                    lastPostId: post.id,
-                    photo100: response.group.photo_100,
-                    name: response.group.name,
-                    screenName: response.group.screen_name
-                });
-
-                console.log(response.group.name);
-            }
+        fetchAPI({
+          groupId: group.id,
+          postId: post.id,
+          text: post.text,
+          date: post.date
         });
 
-        if (newPosts.length) {
-            console.log('Обнаружены новые посты, в группе ' + group.id + ' последний id: ' + group.lastParsedId + '; Новых постов: ' + newPosts.length);
-        } else {
-            // console.log('Новых постов не найдено')
-        }
-    })
+        console.log(post.date);
+
+        group.lastParsedId = post.id;
+
+        console.log("updateLastPost");
+
+        await updateLastPost(group.id, {
+          lastPostId: post.id,
+          photo100: response.group.photo_100,
+          name: response.group.name,
+          screenName: response.group.screen_name
+        });
+
+        console.log(response.group.name);
+      }
+    });
+
+    if (newPosts.length) {
+      console.log(
+        "Обнаружены новые посты, в группе " +
+          group.id +
+          " последний id: " +
+          group.lastParsedId +
+          "; Новых постов: " +
+          newPosts.length
+      );
+    } else {
+      // console.log('Новых постов не найдено')
+    }
+  });
 }
 
 async function run() {
-    await fetchNews();
-    setInterval(async () => await fetchNews(), 60000);
+  await fetchNews();
+  setInterval(async () => await fetchNews(), 60000);
 }
 
-run().catch(console.log).then(console.log);
+run()
+  .catch(console.log)
+  .then(console.log);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,42 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const PrettierPlugin = require("prettier-webpack-plugin");
+const PrettierPlugin = require('prettier-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      styles: path.resolve(__dirname, 'src/styles'),
     },
-    resolve: {
-        alias: {
-            src: path.resolve(__dirname, 'src'),
-            styles: path.resolve(__dirname, 'src/styles')
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    'reshadow/webpack/loader',
-                    'babel-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-      new PrettierPlugin(),
-      new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, 'src/index.html')
-      })
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['reshadow/webpack/loader', 'babel-loader'],
+      },
     ],
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        compress: true,
-        historyApiFallback: true,
-        port: 9000
-    }
+  },
+  plugins: [
+    new PrettierPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 9000,
+  },
 };
